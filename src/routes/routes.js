5e51b66fd5d837af09a06a40543be8b460d77544
dangchien-jpg <nguyendangchien12.5.a@gmail.com>
2025-09-24 11:47:11 +0700

    Xây dựng UI phần Sidebar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,8 +5,10 @@ import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
 import config from '~/config';
+import Live from '~/pages/Live';
 const publicRoutes = [
     { path: config.routes.home, component: Home },
+    { path: config.routes.live, component: Live },
     { path: config.routes.following, component: Following },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
